fe7cddce0e1a3aadb258da4a14d23f3d586ed855
Michael Vilabrera <m.vilabrera@gmail.com>
2018-08-11 16:34:14 -0400

    initial commit

---
+++ Metal000/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  Metal000
+//
+//  Created by Michael Vilabrera on 8/11/18.
+//  Copyright © 2018 Michael Vilabrera. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    @IBOutlet weak var label: NSTextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let devices = MTLCopyAllDevices()
+        guard let _ = devices.first else {
+            fatalError("Your GPU does not support Metal")
+        }
+        
+        label.stringValue = "Your system has the following GPU(s): \n"
+        for device in devices {
+            label.stringValue += "\(device.name)\n"
+        }
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        
+            print("this is new")
+        }
+    }
+
+
+}
+
